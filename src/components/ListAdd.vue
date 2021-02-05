188556c3e3a78f68e80461fcd74dc22d67b5c280
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-05 21:04:52 +0900

    ListAdd.vueファイルの追加

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,15 @@
+<template>
+    <form class="addlist" @submit.prevent="addList">
+        <input v-module = "title"
+            type = "text"
+            class = "text-input"
+            placeholder = "Add new list"
+        >
+        <button type="submit" class="add-button">
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+</script>
