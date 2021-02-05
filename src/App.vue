7edfcb0ea63f423cb0a20b0c871f0121b0200149
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-05 20:51:53 +0900

    コンポーネントの呼び出し

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <board/>
   </div>
 </template>
 
 <script>
 
+import Board from './components/Board.vue'
 
 export default {
   name: 'App',
   components: {
+    Board
   }
 }
 </script>
