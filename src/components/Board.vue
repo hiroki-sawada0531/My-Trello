55888c81b2c8a8b07159ec45c6e41d30cbdec32f
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-05 21:36:09 +0900

    ListAddをコンポーネントで呼び出す

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,11 +5,14 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list-add/>
     </main>
   </div>
 </template>
 
 <script>
+import ListAdd from './ListAdd.vue'
 export default {
+  components: { ListAdd },
 }
 </script>
