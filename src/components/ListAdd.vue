37bbb174d54607d2a4a2ebc4ecc53d3bce97f9d1
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-06 22:43:51 +0900

    フォームが活性の時やテキストが入力された時だけボタンを表示し、フォームにテキストが入力されたら、ボタンが活性になるようにする

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,13 +1,15 @@
 <template>
     <form :class="classList" @submit.prevent="addList">
         <input v-model="title"
-            type="text"
-            class="text-input"
-            placeholder="Add new list"
-            @focusin="startEditing"
-            @focusout="finishEditing"
+            type= "text"
+            class= "text-input"
+            placeholder= "Add new list"
+            @focusin= "startEditing"
+            @focusout= "finishEditing"
         >
-        <button type="submit" class="add-button">
+        <button type= "submit"
+                class= "add-button"
+                v-if= "isEditing||titleExists">
         Add
         </button>
     </form>
@@ -18,18 +20,27 @@ export default {
     data: function() {
         return {
         title:'',
-        isEditing: false,
+        isEditing:false,
         }
     },
-
 computed: {
         classList() {
             const classList = ['addlist']
+
             if(this.isEditing) {
                 classList.push('active')
             }
+
+            if(this.titleExists) {
+                classList.push('addable')
+            }
             return classList
         },
+
+        titleExists() {
+            return this.title.length > 0
+        },
+
     },
 
     methods: {
