45431c0a51229d4955b8e651e63b6e518c3fbe01
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-11 23:50:44 +0900

    カードを作成し、ストアでデータの状態を管理できるように編集

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -1,5 +1,37 @@
 <template>
+    <form class = "addcaed" @submit.prevent = "addCardToList">
+        <input v-model = "body"
+            type = "text"
+            class = "text-input"
+            placeholder= "Add new card"
+            />
+    <button type = "submit" class = "add-button">
+        Add
+    </button>
+    </form>
 </template>
 
 <script>
+export default {
+
+    props: {
+        listIndex: {
+            type: Number,
+            required: true,
+        }
+    },
+
+    data: function() {
+        return {
+            body:'',
+        }
+    },
+
+    methods: {
+        addCardTolist: function() {
+            this.$store.dispatch('addCardTolist', { body: this.body, listIndex: this.listIndex })
+            this.body = ''
+        }
+    }
+}
 </script>
