e35bdabe885345f87768727fb40e66649ae9ec3a
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-12 22:25:40 +0900

    classListをformにバインドさせる

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -4,6 +4,8 @@
             type = "text"
             class = "text-input"
             placeholder= "Add new card"
+            @focussin= "startEditing"
+            @forcusout= "finisiEditing"
             />
     <button type = "submit" class = "add-button">
         Add
@@ -24,10 +26,28 @@ export default {
     data: function() {
         return {
             body:'',
+            isEditing:false,
         }
     },
 
+    computed: {
+        classList() {
+            const classList = ['addcard']
+            if(this.isEditing) {
+                classList.push('active')
+            }
+            return classList
+        },
+    },
+
     methods: {
+        startEditing:function() {
+            this.isEditing = true
+        },
+        finishEditing: function() {
+            this.finishEditing = false
+        },
+
         addCardTolist: function() {
             this.$store.dispatch('addCardTolist', { body: this.body, listIndex: this.listIndex })
             this.body = ''
