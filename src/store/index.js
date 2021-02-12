f7004e8e3dc941eb13ac5c46172b7058d206af0f
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-12 21:59:45 +0900

    フォームボタンが押下されたらカードを新しく作成する仕組みを実装

--- src/store/index.js
+++ src/store/index.js
@@ -35,6 +35,10 @@ const store = new Vuex.Store({
     removelist(state,payload) {
       state.lists.splice(payload.listindex,1)
     },
+
+    addCardToList(state, payload) {
+      state.lists[payload.listindex].cards.push({ body:payload.body})
+    },
   },
   actions:{
     addlist(context, payload){
@@ -43,6 +47,9 @@ const store = new Vuex.Store({
 
     removelist(context, payload) {
       context.commit('removelist', payload)
+    },
+    addCardToList(context,payload) {
+      context.commit('addCardToList', payload)
     },
   },
   getters:{
